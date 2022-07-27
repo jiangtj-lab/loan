63eb9a741e6ec70b8c8bbecf370e91f85b029c71
Mr.J <116749895@qq.com>
2022-07-27 16:51:09 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <BaseHeader />
     <div style="display: flex">
       <div style="width: 100%">
+        <el-row justify="center">
+          <el-link href="https://github.com/jiangtj-lab/loan" target="_blank">GitHub项目地址</el-link>
+        </el-row>
         <LoanCalc />
       </div>
     </div>
